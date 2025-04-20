252f85965099c768d0081b6e2c632f6a7d330877
PauloAlves8039 <pj38alves@gmail.com>
2025-04-20 11:31:55 -0300

    feat: update route file and navbar component

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import Navbar from "../components/Navbar/Navbar";
 import About from "../pages/About/About";
+import QrCodeGenerator from "../components/QrCode/QrCodeGenerator";
 
 export default function AppRoutes() {
     return (
@@ -9,8 +10,9 @@ export default function AppRoutes() {
             <Navbar />
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/qr-code-generator" element={<QrCodeGenerator />} />
                 <Route path="/about" element={<About />} />
-            </Routes>
+            </Routes>        
         </>
     );
 }
