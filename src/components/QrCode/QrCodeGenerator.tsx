083460a7a8c9c2f00e40e33c9744e5c6963ae4ee
PauloAlves8039 <pj38alves@gmail.com>
2025-04-22 11:38:26 -0300

    feat: implement icons and colors in elements

--- src/components/QrCode/QrCodeGenerator.tsx
+++ src/components/QrCode/QrCodeGenerator.tsx
@@ -2,8 +2,9 @@ import { useState } from "react";
 import { useQrCode } from "../../hooks/useQrCode";
 import QrCodeViewer from "./QrCodeViewer";
 import notificationService from "../../utils/notificationService";
-import "../../index.css";
 import LoadingSpinner from "../Loading/LoadingSpinner";
+import { HiQrcode, HiRefresh, HiTrash } from "react-icons/hi";
+import "../../index.css";
 
 export default function QrCodeGenerator() {
     const [text, setText] = useState("");
@@ -32,7 +33,8 @@ export default function QrCodeGenerator() {
         <div className="flex flex-col items-center justify-start px-4 pt-4">
             <div className="bg-white shadow-xl rounded-2xl p-4 w-full max-w-xl flex flex-col items-center border border-gray-300 gap-2">
 
-                <h1 className="text-3xl sm:text-4xl lg:text-3xl font-bold text-blue-700 mb-4 text-center">
+                <h1 className="text-3xl sm:text-4xl lg:text-3xl font-bold text-blue-700 mb-4 text-center flex items-center justify-center gap-2">
+                    <HiQrcode className="w-7 h-7" />
                     QR Code Generator
                 </h1>
 
@@ -47,14 +49,20 @@ export default function QrCodeGenerator() {
                     <button
                         onClick={handleGenerate}
                         disabled={loading}
-                        className="w-full sm:w-28 h-10 bg-blue-600 text-white rounded-md font-semibold hover:bg-blue-700 transition shadow disabled:opacity-50 cursor-pointer"
+                        className="w-full sm:w-28 h-10 bg-green-600 text-white rounded-md font-semibold hover:bg-green-700 transition shadow disabled:opacity-50 cursor-pointer flex items-center justify-center gap-2"
                     >
-                        {loading ? "Generating..." : "Generate"}
+                        {loading ? "Generating..." : (
+                            <>
+                                <HiRefresh className="w-5 h-5" />
+                                Generate
+                            </>
+                        )}
                     </button>
                     <button
                         onClick={handleClear}
-                        className="w-full sm:w-28 h-10 bg-red-600 text-white rounded-md font-semibold hover:bg-red-700 transition shadow cursor-pointer"
+                        className="w-full sm:w-28 h-10 bg-red-600 text-white rounded-md font-semibold hover:bg-red-700 transition shadow cursor-pointer flex items-center justify-center gap-2"
                     >
+                        <HiTrash className="w-5 h-5" />
                         Clear
                     </button>
                 </div>
--- src/components/QrCode/QrCodeViewer.tsx
+++ src/components/QrCode/QrCodeViewer.tsx
@@ -1,4 +1,5 @@
 import { QrCodeViewerProps } from "../../types/QrCodeViewerProps";
+import { HiDownload } from "react-icons/hi";
 
 export default function QrCodeViewer({ imageBase64 }: QrCodeViewerProps) {
     return (
@@ -9,8 +10,9 @@ export default function QrCodeViewer({ imageBase64 }: QrCodeViewerProps) {
                 className="w-full max-w-[150px] sm:max-w-[200px] lg:max-w-[240px] h-auto object-contain rounded-lg border border-gray-300 shadow-md my-2"
             />
             <a href={imageBase64} download="qrcode.png">
-                <button className="mt-4 px-6 py-2 bg-green-600 text-white font-semibold rounded-lg shadow hover:bg-green-700 transition">
-                    Download QR Code
+                <button className="mt-4 px-6 py-2 bg-purple-600 text-white font-semibold rounded-lg shadow hover:bg-purple-700 transition flex items-center cursor-pointer gap-2">
+                    <HiDownload className="w-5 h-5" />
+                    Download
                 </button>
             </a>
         </>
