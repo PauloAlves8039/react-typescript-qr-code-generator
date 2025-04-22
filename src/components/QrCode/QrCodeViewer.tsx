247e5d6f17b688eac361b9a7b212914c26b5e218
PauloAlves8039 <pj38alves@gmail.com>
2025-04-22 12:06:02 -0300

    style: apply animation effect

--- src/components/QrCode/QrCodeViewer.tsx
+++ src/components/QrCode/QrCodeViewer.tsx
@@ -7,7 +7,7 @@ export default function QrCodeViewer({ imageBase64 }: QrCodeViewerProps) {
             <img
                 src={imageBase64}
                 alt="QR Code"
-                className="w-full max-w-[150px] sm:max-w-[200px] lg:max-w-[240px] h-auto object-contain rounded-lg border border-gray-300 shadow-md my-2"
+                className="w-full max-w-[150px] sm:max-w-[200px] lg:max-w-[240px] h-auto object-contain rounded-lg border border-gray-300 shadow-md my-2 animation-bounce-in-2s"
             />
             <a href={imageBase64} download="qrcode.png">
                 <button className="mt-4 px-6 py-2 bg-purple-600 text-white font-semibold rounded-lg shadow hover:bg-purple-700 transition flex items-center cursor-pointer gap-2">
